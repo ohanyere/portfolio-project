2935d8b7efe9943afbd1f0b7e788e30f29f86870
ohanyere <mooref068@gmail.com>
2023-04-05 21:53:00 -0700

    Resets the game board to restart the game

--- reset_board.py
+++ reset_board.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-'''Defines the logic behind the coloration of the board cells'''
+'''Defines the logic that resets the game board to restart game'''
 import tkinter as kin
 from itertools import cycle
 from tkinter import font
@@ -15,20 +15,23 @@ from tic_tac_toe_board import create_menu
 from valid_move import is_move_valid
 from get_winning import get_winning_combination
 from processing_move import process_move
+from reset_game import game_reset
 from is_winner import any_winner
 from game_tied import is_tied
 from alternate_player import switch_player
 from creating_board_display import create_board_display
 from creating_grid import create_board_grid
-from reset_board import board_reset
 from game_play import play
 from update_button import button_update
 from update_display import display_update
-from reset_game import game_reset
+from highlight_cells import cells_highlight
 
 
-def cells_highlight(self):
-    '''Handles the coloration of the board cells'''
-    for button, coordinates in self.cells.items():
-        if coordinates in self.game.winner_combination:
-            button.config(highlightbackground="red")
+def board_reset(self):
+    '''Resets the game board to restart the game'''
+    self.game.game_reset()
+    self.display_update(msg="Game On")
+    for button in self.cells.keys():
+        button.config(highlightbackground="orange")
+        button.config(text="")
+        button.config(fg="blue")
