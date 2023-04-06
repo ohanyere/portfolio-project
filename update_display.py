df08d539ad18ecf7721d0b1c5a605c9c0469f0fa
ohanyere <mooref068@gmail.com>
2023-04-05 22:14:52 -0700

    Displays message and color update on the board

---
+++ update_display.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+'''Defines the logic behind the display of message and color on the board'''
+import tkinter as kin
+from itertools import cycle
+from tkinter import font
+from typing import NamedTuple as NT
+from tic_tac_toe_player import Player
+from tic_tac_toe_move import Move
+from tic_tac_toe_game import Game
+from tic_tac_toe_game import board_setup
+from tic_tac_toe_game import get_name
+from tic_tac_toe_board import Board
+from tic_tac_toe_board import create_menu
+from valid_move import is_move_valid
+from get_winning import get_winning_combination
+from processing_move import process_move
+from is_winner import any_winner
+from game_tied import is_tied
+from alternate_player import switch_player
+from creating_board_display import create_board_display
+from creating_grid import create_board_grid
+from reset_board import board_reset
+from game_play import play
+from update_button import button_update
+from reset_game import game_reset
+from highlight_cells import cells_highlight
+
+
+def display_update(self, msg, color="blue"):
+    '''Displays message and color update on the board'''
+    self.display["text"] = msg
+    self.display["fg"] = color
